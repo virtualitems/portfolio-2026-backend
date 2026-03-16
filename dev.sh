c7eee03c6b27534b516d7a10d7df9a0600352ded
Alejandro CR <contacto@alejandrocr.co>
2026-03-16 06:15:42 +0000

    replace uvicorn with hypercorn

--- dev.sh
+++ dev.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir -p /var/run/demo.alejandrocr.co && sudo chown ec2-user:ec2-user /var/run/demo.alejandrocr.co
 
-sudo -u ec2-user $(pwd)/venv/bin/uvicorn server.main:app --uds /var/run/demo.alejandrocr.co/unix.sock --reload --proxy-headers --app-dir $(pwd) --lifespan off --log-level debug
+cd $(pwd) && sudo -u ec2-user $(pwd)/venv/bin/hypercorn server.main:app --bind unix:/var/run/demo.alejandrocr.co/unix.sock --reload --debug
 
 # sudo docker run redis:alpine --name redis -p 6379:6379
