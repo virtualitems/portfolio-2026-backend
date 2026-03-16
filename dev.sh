e22a65fa64e0424a3adc57337e890c132be10508
Alejandro CR <contacto@alejandrocr.co>
2026-03-16 08:28:36 +0000

    fix: update dev.sh to remove existing socket, set directory permissions, and launch hypercorn

--- dev.sh
+++ dev.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sudo mkdir -p /var/run/demo.alejandrocr.co && sudo chown ec2-user:ec2-user /var/run/demo.alejandrocr.co
+# Eliminar cualquier socket existente
+sudo rm -f /var/run/demo.alejandrocr.co/unix.sock && \
+# Crear directorio con permisos adecuados
+sudo mkdir -p /var/run/demo.alejandrocr.co && \
+sudo chown ec2-user:nginx /var/run/demo.alejandrocr.co && \
+sudo chmod 775 /var/run/demo.alejandrocr.co && \
+# Lanzar hypercorn (que creará el socket automáticamente)
+sudo -u ec2-user $(pwd)/venv/bin/python -m hypercorn server.main:app --bind unix:/var/run/demo.alejandrocr.co/unix.sock --umask 113 --reload --debug
 
-cd $(pwd) && sudo -u ec2-user $(pwd)/venv/bin/hypercorn server.main:app --bind unix:/var/run/demo.alejandrocr.co/unix.sock --reload --debug
+namei -nom /var/run/demo.alejandrocr.co/unix.sock
 
 # sudo docker run redis:alpine --name redis -p 6379:6379
