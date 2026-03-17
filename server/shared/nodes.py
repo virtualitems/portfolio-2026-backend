475db2515768d4eaf3babbd31304a5648fdd0810
Alejandro CR <contacto@alejandrocr.co>
2026-03-17 01:54:13 +0000

    feat: add base agent and runnable node classes for processing inputs and generating responses

---
+++ server/shared/nodes.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from typing import AsyncIterator, Union
+from abc import ABC, abstractmethod
+
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, SystemMessage
+from langchain_ollama import ChatOllama
+
+class BaseAgentNode(ABC):
+    """
+    Nodo base para los agentes.
+    """
+
+    def __init__(self, llm: ChatOllama, system_prompt: Union[None, str] = None):
+        self.llm = llm
+        self.system_prompt = system_prompt or ''
+
+
+class BaseRunnableAgentNode(BaseAgentNode):
+    """
+    Nodo que puede procesar un input y generar una respuesta.
+    """
+    @abstractmethod
+    async def run(self, input_text: str) -> str:
+        """
+        Procesa el input y retorna la respuesta.
+        """
+        pass
+
+class BaseRunnableStreamAgentNode(BaseAgentNode):
+    """
+    Nodo que puede procesar un input y generar una respuesta en streaming.
+    """
+    @abstractmethod
+    async def run_stream(self, input_text: str) -> AsyncIterator[str]:
+        """
+        Procesa el input y retorna la respuesta como un stream de texto.
+        """
+        pass
+
+
+class BaseRunnableNode:
+    """
+    Nodo que puede procesar un input y generar una respuesta.
+    """
+    @abstractmethod
+    async def run(self) -> str:
+        """
+        Procesa el input y retorna la respuesta.
+        """
+        pass
+
+class BaseRunnableStreamNode:
+    """
+    Nodo que puede procesar un input y generar una respuesta en streaming.
+    """
+    @abstractmethod
+    async def run_stream(self) -> AsyncIterator[str]:
+        """
+        Procesa el input y retorna la respuesta como un stream de texto.
+        """
+        pass
